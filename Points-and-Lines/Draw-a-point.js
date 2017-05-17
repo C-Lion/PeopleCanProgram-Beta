f1e5024722682a84df6427f9cd15d29893335703
C-Lion <esstepansky@gmail.com>
2017-05-17 17:39:12 +0300

    Create PeopleCanProgram-Beta Github repo

---
+++ Points-and-Lines/Draw-a-point.js
@@ -0,0 +1,13 @@
+// LESSON NAME: Draw a point.
+// This line of code is a comment.
+// Comments don't do anything, they just make a program easier to read and understand.
+
+// Create a Point object.
+var p = new Point();
+// Set the x-coordinate of p.
+p.x = 100;
+// Define y-coordinate of p.
+p.y = 50;
+// Draw point p.
+p.draw();
+// Lesson is complete and correct :) 
